0cc12a7d1b083f948b900903cf0afdb7c7f12731
kanugurajesh <kanugurajesh3@gmail.com>
2024-02-12 23:19:26 +0530

    Adding image changes

--- app/post/[serialNo]/page.tsx
+++ app/post/[serialNo]/page.tsx
@@ -14,7 +14,6 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default function Posts({ params }: { params: { serialNo: number } }) {
-
   const randomName: string = uniqueNamesGenerator({
     dictionaries: [adjectives, colors, animals],
   });
@@ -109,7 +108,6 @@ export default function Posts({ params }: { params: { serialNo: number } }) {
     } else {
       toast.error("Failed to add message");
     }
-
   };
 
   const handleContent = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -143,25 +141,30 @@ export default function Posts({ params }: { params: { serialNo: number } }) {
         {/* @ts-ignore */}
         {post.content && <p>{post.content}</p>}
         {/* @ts-ignore */}
-        {post.image && (<Image src={post.image} alt="Image" width={200} height={200} />
+        {post.image && (
+          // @ts-ignore
+          <Image src={post.image} alt="Image" width={200} height={200} />
         )}
       </div>
       <div>
         <h1 className="text-2xl font-black my-5 mb-10">Messages</h1>
         {messages.map((message: any, index: number) => {
           return (
-            <div key={index} className="flex flex-col gap-3 mt-6 bg p-2 bg-black text-white">
+            <div key={index} className="flex flex-col gap-3 mt-6 bg p-2 border-2 border-black rounded-md p-3">
               <div className="flex items-center gap-3">
-                <Image
+                <img
                   src={message.profile}
                   alt="Profile"
-                  width={60}
-                  height={60}
-                  className="rounded-sm"
+                  style={{
+                    width: "60px",
+                    height: "60px",
+                    borderRadius: "50%",
+                    objectFit: "cover",
+                  }}
                 />
                 <h1 className="text-lg font-bold">{message.name}</h1>
               </div>
-              <p className="font-medium">{message.content}</p>
+              <p className="font-medium pl-2">{message.content}</p>
               {message.image && (
                 <Image
                   src={message.image}
@@ -194,7 +197,10 @@ export default function Posts({ params }: { params: { serialNo: number } }) {
                 toast.error(`ERROR! ${error.message}`);
               }}
             />
-            <button className="bg-black text-white font-medium text-lg p-1 rounded-sm px-2 hover:bg-white hover:text-black border-black border-2 transition duration-300 ease-in-out mb-6" onClick={handleMessage}>
+            <button
+              className="bg-black text-white font-medium text-lg p-1 rounded-sm px-2 hover:bg-white hover:text-black border-black border-2 transition duration-300 ease-in-out mb-6"
+              onClick={handleMessage}
+            >
               Add
             </button>
           </div>
